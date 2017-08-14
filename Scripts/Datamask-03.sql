ae3b698767a7e02747b0d5ef73d680fab4f7ee75
Renato Groffe <renato.groffe@yahoo.com.br>
2017-08-13 21:13:56 -0300

    Data Masking no SQL Server 2017
    Utilizando Data Masks com o SQL Server 2017 em uma aplicação ASP.NET
    Core 1.1 + Dapper

---
+++ Scripts/Datamask-03.sql
@@ -0,0 +1,5 @@
+CREATE LOGIN testedatamasks
+    WITH PASSWORD = 'TesteDataMasks01!';
+
+CREATE USER testedatamasks FOR LOGIN testedatamasks;
+GO
---
+++ SiteDataMasking/SiteDataMasking/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Data.SqlClient;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Dapper;
+
+namespace SiteDataMasking.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index(
+            [FromServices]IConfiguration config)
+        {
+            List<Cliente> clientes;
+
+            using (SqlConnection conexao =
+                new SqlConnection(
+                    config.GetConnectionString("ExemploDataMask")))
+            {
+                clientes = conexao.Query<Cliente>(
+                    "SELECT * " +
+                    "FROM dbo.Clientes").AsList();
+            }
+
+            return View(clientes);
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
